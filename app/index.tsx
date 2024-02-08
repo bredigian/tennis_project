a4e153c7958a8c7f646af8d6cbb916c98a4aa898
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2024-02-08 16:17:10 -0300

    [init] initialized react native app with Expo Router, TypeScript and NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native"
+
+import React from "react"
+
+const Home = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-center bg-blue-400 p-4 rounded-2xl font-medium">
+        React Native App with Expo Router, TypeScript and NativeWind for
+        TailwindCSS
+      </Text>
+    </View>
+  )
+}
+
+export default Home
